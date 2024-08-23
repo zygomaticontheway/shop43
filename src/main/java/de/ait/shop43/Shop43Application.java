06af05eb87e0b5d7d66852a160f7f486e8a6c319
zygomaticontheway <mrzygomatic@gmail.com>
2024-08-23 10:24:07 +0200

    shop43 init

---
+++ src/main/java/de/ait/shop43/Shop43Application.java
@@ -0,0 +1,13 @@
+package de.ait.shop43;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Shop43Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Shop43Application.class, args);
+    }
+
+}
